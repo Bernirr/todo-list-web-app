f86b82d7461ca449f3c137663bfdc476e7444087
Bernard Felix <flixzz44@gmail.com>
2024-09-06 22:18:28 +0200

    From Day 19c: web_app.py: - Changed page layout, - used html - implemented multiple pages - added comments (20/09/24)

---
+++ pages/about.py
@@ -0,0 +1,4 @@
+import  streamlit as st
+
+st.write("Hello and welcome to my minimalisticikikical todo-list app." "\n"
+         "She's gonna make you more <b>productive</b>.", unsafe_allow_html=True)
